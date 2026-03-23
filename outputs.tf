82ca06240b993ade09468707cbdd7e9ec1c11806
sokie <havelka@centrum.cz>
2026-03-23 14:49:02 +0100

    updated outpu of terrafom

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "network_name" {
   value       = nutanix_subnet.phys_lan.name
   description = "The name of the created network"
 }
+
+output "vm_id" {
+  value       = nutanix_virtual_machine.vm1.id
+  description = "The UUID of the created Virtual Machine"
+}
+
+output "vm_name" {
+  value       = nutanix_virtual_machine.vm1.name
+  description = "The name of the created Virtual Machine"
+}
