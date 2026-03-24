ed81729bbd905a0a005bafaabbfe881065c76018
xhavel17 <havelka.du@gmail.com>
2026-03-24 09:03:17 +0100

    Configure remote backend and cloud settings
    Added remote backend configuration for Terraform.

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,23 @@
 terraform {
   required_version = ">= 1.0.0"
+  cloud { 
+    hostname = "terrakube-api.rak.farnostrakvice.cz"
+    organization = "Testing" 
+
+    workspaces { 
+      name = "TEST-CLI" 
+    } 
+  } 
+terraform { 
+  backend "remote" { 
+    hostname = "terrakube-api.rak.farnostrakvice.cz"
+    organization = "Testing" 
+
+    workspaces { 
+      name = "TEST-CLI" 
+    } 
+  } 
+} 
   required_providers {
     nutanix = {
       source  = "nutanix/nutanix"
